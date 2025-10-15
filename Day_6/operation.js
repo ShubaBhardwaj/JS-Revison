e8fde7467955925fce18cbfc6231ff8ae331ebce
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2025-10-15 21:30:05 +0530

    Day 6

---
+++ Day_6/operation.js
@@ -0,0 +1,14 @@
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+
+console.log("Addition of two strings...",str3);
+
+console.log("1"+2);
+console.log(1+2+"2");
+
+// In this "console.log("1"+2+2)" condition string comes first so addition will be done as string concatenation 
+// In this "console.log(1+2+"2")" condition number comes first so addition will be done as number and then string concatenation
+
+
+
